8c3c88876b0f69d91cdb4f9f819672134940cedc
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-10-17 05:02:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let latitude = 22.7868542, longitude = 88.3643296;
+
+// Initializing Mapbox
+
+
+
+
+//Add Mapbox Geocoder
+
+
+
+
+
+var img1 = document.querySelector("#amber")
+
+// Create a Amber Palace, Jaipur Marker and add it to the map.
+
+// Create a  Gateway of India, Mumbai Marker and add it to the map.
+
+// Create a India Gate Marker and add it to the map.
+
+
+// Create a Lotus Temple, Delhi Marker and add it to the map.
+
+
+//Create a Victoria Memorial, Kolkata Marker and add it to the map.
